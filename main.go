72d07c77040e25fbf73e17d708d0d367b2402158
kjuiop <copying_y@naver.com>
2024-05-29 19:02:29 +0900

    [#1] feat: gin framework apply

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello file uploader app")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8080")
 }
